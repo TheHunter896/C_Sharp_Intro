5e052213b72e9504e2d63995565f2a3dba25e4a8
--replace <david_corp896@outlook.es>
2020-06-01 19:18:11 +0200

    Updated

--- c_sharp_course_udemy/dataTypes.cs
+++ c_sharp_course_udemy/dataTypes.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Linq;
 
-namespace c_sharp_course_udemy
-{
-    public class data_types
+namespace CsCourseUdemy
+{   
+    public class DataTypes
     {
        
 
@@ -11,6 +12,7 @@ namespace c_sharp_course_udemy
 
             //Variables in C# do not interact directly with memory, they get initialized as 0 as default.
             //Variables are also initialized like python, can't use {} like in c++
+            //However the reference is still accessible.
 
             //Ints
             int age = 15; //int reaches 2^32
@@ -43,13 +45,47 @@ namespace c_sharp_course_udemy
             Console.WriteLine("Age is: " + character);
             Console.WriteLine("Age is: " + str);
 
-            Console.Read(); //If you dont add this console won't display the writes.
+            string ascii_x = Console.ReadLine(); //Returns the ascii reference of what is typed
+            //Use int variable + Read to get the string value in ASCII
+            //Use string + ReadLine to get the actual value in string (of wha the user inputs)
+            Console.WriteLine(ascii_x);
         }
 
-        public static void arithmetic_operations()
+        public static void VariableConversion()
         {
+            double myDouble = 13.38;
+            int myInt;
+
+            //Explicit conversion
+            //Cannot asign directly to int, need round/ceil, or cast
+            //Use C-Like conversion to change data types between numbers
+            myInt = (int)myDouble; //As you can see this is how it's done in C, in C++ we would use a static_cast() function
+
+            int num = 1323435;
+            long bigNum = num; //Implicit conversion in the value of num
             
+            //From int/double/float/decimal to string
+            //use methods to change from number to string
+            int num2 = 3;
+            string num2String = num2.ToString();
+            // string num2String2 = (string)num2; //Cannot use C-Like conversion
+
+            //From string to number, use this with the according class you want to transform to
+            string stringToInt = "14";
+            int parsedInt = Int32.Parse(stringToInt);
+
         }
+
+        public static void stringMethods()
+        {
+            string exampleString = "David";
+            exampleString.ToLower();
+            exampleString.ToUpper();
+            exampleString.IndexOf('d');
+            exampleString.Substring(0, 1);
+            String.Format(exampleString, "replacement"); //The string needs a {} with the index to be reformatted
+
+        }   
     }
 
 }
---
+++ c_sharp_course_udemy/methodsAndFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsCourseUdemy
+{
+    class FunctionsAndMethods
+    {
+        public void method1()  //Parameters need 
+        {
+            //Methods work exactly like C++, but they don't need all the implementation that C++ does with .h and .cpp
+            //Which makes it a little bit clearer, but more clustered in the cs files
+
+            //The structure is [accessType] [static] returnType methodName (parameterList)
+            //Static is basically the cls in Python, does not belong to an instance, but the whole class definition.
+        }
+    }
+}
