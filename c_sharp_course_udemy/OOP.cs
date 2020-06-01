38982f5dcf635b4e472151150826c55d5a3b5fb2
--replace <david_corp896@outlook.es>
2020-06-01 21:13:27 +0200

    Updated arrays, added OOP

---
+++ c_sharp_course_udemy/OOP.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsCourseUdemy
+{
+    class OOP
+    {
+        //OOP
+    }
+}
--- c_sharp_course_udemy/arrays.cs
+++ c_sharp_course_udemy/arrays.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,13 +9,7 @@ namespace CsCourseUdemy
     {
         public void arraysMain()
         {
-            //You can basically declare arrays with a dataType and specifying the array after -> int[] intArray;
-            //int[] name = [initialization]
-            //int[] name = {1, 2, 3, 4, 5}
-            //int[] name = new int[4]; -> Initializes 4 spaces, empty
-            //If you want to add values to the previous initialization remember that it has to have the same length
-            //int[] name = new int[4] { 1, 2, 3, 4 }; -> allocation and init are the same length
-
+           
             //Lists
             //Lists are contiguous indefinite allocation. Use () after list to say which size, use {} to initialize values 
             List<int> example = new List<int>() { }; //Use a capacity integers in parathesis, dont use it to have unlimited
@@ -24,6 +19,21 @@ namespace CsCourseUdemy
 
             //You can also declare generic type arrays lists
             var generic = new List<string>() { };
+
+            //Arraylists
+            //You can also declare lists like so, but this is depracated
+            ArrayList myArrayList = new ArrayList();
+            myArrayList.Add(1);
+            myArrayList.Add("Hi");
+            //When looping we should use var or object.
+
+            //Arrays
+            //You can basically declare arrays with a dataType and specifying the array after -> int[] intArray;
+            //int[] name = [initialization]
+            //int[] name = {1, 2, 3, 4, 5}
+            //int[] name = new int[4]; -> Initializes 4 spaces, empty
+            //If you want to add values to the previous initialization remember that it has to have the same length
+            //int[] name = new int[4] { 1, 2, 3, 4 }; -> allocation and init are the same length
 
             //Multidimensional arrays are also supported
             int[,] arrayName = new int[,] { { }, { } }; // 1 comma, 2d, 2 commas, 3d, etc.
@@ -45,6 +55,10 @@ namespace CsCourseUdemy
             jaggedArray[1] = new int[3];
             jaggedArray[2] = new int[2];
         }
+
+        //Arrays are non generic (tuples in python)
+        //ArrayLists the depcrataded lists
+        //Lists are generic and you dont have to specify the length (lists in python)
 
     }
 }
